a0aec4e9f2147ac15fa578b3df0ddc52e8c00507
6uu1 <122669698+6uu1@users.noreply.github.com>
2023-03-30 16:07:27 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'CHATGPT',
+      description: 'only for study! 仅供内部学习交流使用！',
     },
   }
 }
